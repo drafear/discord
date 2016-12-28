253b9deaef09b9d2e83571dfdaf75d09b63250e6
drafear <drafear@github.com>
2016-12-28 21:17:17 +0900

    bitbucket push イベントに対応

--- bitbucket/js/bitbucket.js
+++ bitbucket/js/bitbucket.js
@@ -2,21 +2,32 @@
 
 const g = {
     lib: require('./lib.js'),
+    settings: require('./settings.js'),
     bitbucketPayload: require('bitbucket-payload'),
     hapi: require('hapi'),
     port: +(process.env.OPENSHIFT_NODEJS_PORT || '8080'),
-    ip: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+    ip: null,
     client: null,
     server: null,
     prefix: "[Bitbucket]",
+    targetChannel: null,
+    eventEmitter: require('events'),
+    emitter: null,
 };
+g.ip = process.env.OPENSHIFT_NODEJS_IP || g.lib.getLocalAddress(["ローカル エリア接続", "ワイヤレス ネットワーク接続"]).ipv4.address;
+g.emitter = new g.eventEmitter();
+
 const log = (msg) => {
     g.lib.log(g.client, g.prefix, msg);
 };
 const error = (msg) => {
     g.lib.error(g.client, g.prefix, msg);
 };
+const say = (msg) => {
+    g.targetChannel.sendMessage(msg);
+};
 const init = () => {
+    g.targetChannel = g.client.getTextChannel(g.settings.isTest ? "test" : "bitbucket");
     g.server = new g.hapi.Server();
     g.server.connection({
         port: g.port,
@@ -27,11 +38,22 @@ const init = () => {
         path: '/bitbucket',
         handler: (request, reply) => {
             reply().code(204);
-            log(request.payload);
+            const payload = request.payload;
+            if (!payload) return; // webhook以外の場合
+            if (!request.headers["x-event-key"]) return;
+            g.emitter.emit(request.headers["x-event-key"], payload);
         },
     });
     g.server.start(() => {
         log(`Server is Running at: ${g.server.info.uri}`);
+    });
+    g.emitter.on('repo:push', (payload) => {
+        for (const change of payload.push.changes) {
+            for (const commit of change.commits) {
+                log(`New Commit ${commit.hash} By ${commit.author.raw}`);
+                say(`New Commit ${commit.hash} By ${commit.author.raw}\n\`\`\`${commit.message}\`\`\`\n${commit.links.html.href}`);
+            }
+        }
     });
 };
 exports.run = (client) => {
--- bitbucket/js/lib.js
+++ bitbucket/js/lib.js
@@ -3,8 +3,47 @@
 const g = {
     sleep: require('promise.sleep'),
     settings: require('./settings.js'),
+    os: require('os'),
 };
 
+exports.getLocalAddress = (priority) => {
+    const ifacesObj = {
+        ipv4: [],
+        ipv6: [],
+    };
+    const interfaces = g.os.networkInterfaces();
+    for (const dev in interfaces) {
+        interfaces[dev].forEach((details) => {
+            if (!details.internal) {
+                switch (details.family) {
+                case "IPv4":
+                    ifacesObj.ipv4.push({name:dev, address:details.address});
+                    break;
+                case "IPv6":
+                    ifacesObj.ipv6.push({name:dev, address:details.address});
+                    break;
+                }
+            }
+        });
+    }
+    if (priority !== undefined) {
+        const convert = (iface) => {
+            for (const network_name of priority) {
+                for (const network of iface) {
+                    if (network.name === network_name) {
+                        return network;
+                    }
+                }
+            }
+            if (iface.length == 0) return null;
+            return iface[0];
+        }
+        for (const key in ifacesObj) {
+            ifacesObj[key] = convert(ifacesObj[key]);
+        }
+    }
+    return ifacesObj;
+};
 exports.gc = () => {
     if ("gc" in global) {
         global.gc();
